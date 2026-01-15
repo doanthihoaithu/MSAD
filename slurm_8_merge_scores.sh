4654899e5e43b636d4dd899dfcfe498d15cda520
macbook <hoaithuxt1998@gmail.com>
2026-01-16 00:38:39 +0100

    update slurm script for merging scores

--- slurm_8_merge_scores.sh
+++ slurm_8_merge_scores.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=Split_train_test
+#SBATCH --job-name=merge_scores
 #SBATCH --nodes=1 # 1 node
 #SBATCH --ntasks-per-node=16 # 32 tasks per node
 #SBATCH --time=24:00:00 # time limits: 1 hour
-#SBATCH --error=slurm_logs/split_train_test.err # standard error file
-#SBATCH --output=slurm_logs/split_train_test.out # standard output file
+#SBATCH --error=slurm_logs/merge_scores.err # standard error file
+#SBATCH --output=slurm_logs/merge_scores.out # standard output file
 #SBATCH --gres=gpu:a100:1
 #SBATCH --partition=gprod_gssi # partition name
 #SBATCH --mail-type=END              # type of event notification
@@ -12,5 +12,5 @@
 
 module load python
 source /home/doan/projects/ibm_scripts/torch210/bin/activate
-python split_train_test_data.py
+python merge_scores.py
 deactivate
