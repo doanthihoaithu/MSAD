5e8ad9b89508e6bbf0ab24cf0ac6fe68bce716c1
Thu HPC <thihoaithu.doan@gssi.it>
2026-01-15 17:25:32 +0100

    ignore slurm logs

--- slurm_create_windowed_dataset.sh
+++ slurm_create_windowed_dataset.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=Create windowed dataset for synthetic data
+#SBATCH --job-name=Create_windowed_dataset
 #SBATCH --nodes=1 # 1 node
 #SBATCH --ntasks-per-node=16 # 32 tasks per node
 #SBATCH --time=24:00:00 # time limits: 1 hour
-#SBATCH --error=create_windows.err # standard error file
-#SBATCH --output=create_windowes.out # standard output file
+#SBATCH --error=slurm_logs/create_windows.err # standard error file
+#SBATCH --output=slurm_logs/create_windows.out # standard output file
 #SBATCH --gres=gpu:a100:1
 #SBATCH --partition=gprod_gssi # partition name
 #SBATCH --mail-type=END              # type of event notification
