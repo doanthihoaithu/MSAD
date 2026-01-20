4b25ff0413f7977486de16f21c55b867b156764a
Thu HPC <thihoaithu.doan@gssi.it>
2026-01-20 23:15:49 +0100

    update slurm script

--- slurm_6_train_deep_models.sh
+++ slurm_6_train_deep_models.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=24:00:00 # time limits: 1 hour
 #SBATCH --error=slurm_logs/train_deep_model.err # standard error file
 #SBATCH --output=slurm_logs/train_deep_model.out # standard output file
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:a100
 #SBATCH --partition=gprod_gssi # partition name
 #SBATCH --mail-type=END              # type of event notification
 #SBATCH --mail-user=thihoaithu.doan@gssi.it   # mail address
