39a9a66e6774b9b8abf626aa41bb3b92117c0339
macbook <hoaithuxt1998@gmail.com>
2026-01-27 00:20:04 +0100

    run 3 models of transformer

--- eval_combine_detectors.py
+++ eval_combine_detectors.py
@@ -110,7 +110,7 @@ def eval_combine_multiple_detectors(combine_detector_evaluation_config,
 				model_names = ['resnet_default', 'convnet_default', 'inception_time_default']
 			elif running_model_family == 'transformer':
 				# model_names = ['sit_conv_patch', 'sit_linear_patch', 'sit_stem_original', 'sit_stem_relu']
-				model_names = ['sit_stem_relu']
+				model_names = ['sit_conv_patch', 'sit_linear_patch', 'sit_stem_original']
 			elif running_model_family == 'feature_based':
 				model_names = list(classifiers.keys())
 				# model_names = ['qda']
