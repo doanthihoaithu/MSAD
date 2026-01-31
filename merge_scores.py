a270e422b3b69c2aa5edbdef7a3ac89f32b713f8
macbook <hoaithuxt1998@gmail.com>
2026-01-31 19:43:18 +0100

    update merge_score

--- merge_scores.py
+++ merge_scores.py
@@ -22,67 +22,67 @@ import numpy as np
 from natsort import natsorted
 
 
-def merge_scores(path, metric, save_path):
-	# Load MetricsLoader object
-	metricsloader = MetricsLoader(TSB_metrics_path)
-	
-	# Check if given metric exists
-	if metric.upper() not in metricsloader.get_names():
-		raise ValueError(f"Not recognizable metric {metric}. Please use one of {metricsloader.get_names()}")
-
-	# Read accuracy table, fix filenames & indexing, remove detectors scores
-	acc_tables_path = os.path.join(TSB_acc_tables_path, "mergedTable_AUC_PR.csv")
-	acc_tables = pd.read_csv(acc_tables_path)
-	acc_tables['filename'] = acc_tables['filename'].apply(lambda x: x.replace('.txt', '.out') if x.endswith('.txt') else x)
-	acc_tables.set_index(['dataset', 'filename'], inplace=True)
-	acc_tables.drop(columns=univariate_detector_names, inplace=True)
-
-	# Read detectors and oracles scores
-	metric_scores = metricsloader.read(metric.upper())
-	
-	# Read classifiers predictions, and add scores
-	df = None
-	scores_files = [x for x in os.listdir(path) if '.csv' in x]
-	for file in scores_files:
-		file_path = os.path.join(path, file)
-		current_classifier = pd.read_csv(file_path, index_col=0)
-		col_name = [x for x in current_classifier.columns if "class" in x][0]
-		
-		values = np.diag(metric_scores.loc[current_classifier.index, current_classifier.iloc[:, 0]])
-		curr_df = pd.DataFrame(values, index=current_classifier.index, columns=[col_name.replace("_class", "")])
-		curr_df = pd.merge(current_classifier[col_name], curr_df, left_index=True, right_index=True)
-		
-		if df is None:
-			df = curr_df
-		else:
-			df = pd.merge(df, curr_df, left_index=True, right_index=True)
-	df = df.reindex(natsorted(df.columns, key=lambda y: y.lower()), axis=1)
-
-	# Add Oracle (TRUE_ORACLE-100) and Averaging Ensemble
-	df = pd.merge(df, metric_scores[["TRUE_ORACLE-100", "AVG_ENS"] + univariate_detector_names], left_index=True, right_index=True)
-	df.rename(columns={'TRUE_ORACLE-100': 'Oracle', 'AVG_ENS': 'Avg Ens'}, inplace=True)
-	
-	# Add true labels from AUC_PR metrics
-	auc_pr_detectors_scores = metricsloader.read('AUC_PR')[univariate_detector_names]
-	labels = auc_pr_detectors_scores.idxmax(axis=1).to_frame(name='label')
-	df = pd.merge(labels, df, left_index=True, right_index=True)
-	
-	# Change the indexes to dataset, filename
-	old_indexes = df.index.tolist()
-	old_indexes_split = [tuple(x.split('/')) for x in old_indexes]
-	filenames_df = pd.DataFrame(old_indexes_split, index=old_indexes, columns=['dataset', 'filename'])
-	df = pd.merge(df, filenames_df, left_index=True, right_index=True)
-	df = df.set_index(keys=['dataset', 'filename'])
-
-	# Merge the two dataframes now that they have common indexes
-	final_df = df.join(acc_tables)
-	indexes_not_found = final_df[final_df.iloc[:, -len(acc_tables.columns):].isna().any(axis=1)].index.tolist()
-	print('Indexes not found in acc_tables file:')
-	[print(i, x) for i, x in enumerate(indexes_not_found)]
-
-	# Save the final dataframe
-	final_df.to_csv(os.path.join(save_path, f'current_accuracy_{metric.upper()}.csv'), index=True)
-	print(final_df)
+# def merge_scores(path, metric, save_path):
+# 	# Load MetricsLoader object
+# 	metricsloader = MetricsLoader(TSB_metrics_path)
+#
+# 	# Check if given metric exists
+# 	if metric.upper() not in metricsloader.get_names():
+# 		raise ValueError(f"Not recognizable metric {metric}. Please use one of {metricsloader.get_names()}")
+#
+# 	# Read accuracy table, fix filenames & indexing, remove detectors scores
+# 	acc_tables_path = os.path.join(TSB_acc_tables_path, "mergedTable_AUC_PR.csv")
+# 	acc_tables = pd.read_csv(acc_tables_path)
+# 	acc_tables['filename'] = acc_tables['filename'].apply(lambda x: x.replace('.txt', '.out') if x.endswith('.txt') else x)
+# 	acc_tables.set_index(['dataset', 'filename'], inplace=True)
+# 	acc_tables.drop(columns=univariate_detector_names, inplace=True)
+#
+# 	# Read detectors and oracles scores
+# 	metric_scores = metricsloader.read(metric.upper())
+#
+# 	# Read classifiers predictions, and add scores
+# 	df = None
+# 	scores_files = [x for x in os.listdir(path) if '.csv' in x]
+# 	for file in scores_files:
+# 		file_path = os.path.join(path, file)
+# 		current_classifier = pd.read_csv(file_path, index_col=0)
+# 		col_name = [x for x in current_classifier.columns if "class" in x][0]
+#
+# 		values = np.diag(metric_scores.loc[current_classifier.index, current_classifier.iloc[:, 0]])
+# 		curr_df = pd.DataFrame(values, index=current_classifier.index, columns=[col_name.replace("_class", "")])
+# 		curr_df = pd.merge(current_classifier[col_name], curr_df, left_index=True, right_index=True)
+#
+# 		if df is None:
+# 			df = curr_df
+# 		else:
+# 			df = pd.merge(df, curr_df, left_index=True, right_index=True)
+# 	df = df.reindex(natsorted(df.columns, key=lambda y: y.lower()), axis=1)
+#
+# 	# Add Oracle (TRUE_ORACLE-100) and Averaging Ensemble
+# 	df = pd.merge(df, metric_scores[["TRUE_ORACLE-100", "AVG_ENS"] + univariate_detector_names], left_index=True, right_index=True)
+# 	df.rename(columns={'TRUE_ORACLE-100': 'Oracle', 'AVG_ENS': 'Avg Ens'}, inplace=True)
+#
+# 	# Add true labels from AUC_PR metrics
+# 	auc_pr_detectors_scores = metricsloader.read('AUC_PR')[univariate_detector_names]
+# 	labels = auc_pr_detectors_scores.idxmax(axis=1).to_frame(name='label')
+# 	df = pd.merge(labels, df, left_index=True, right_index=True)
+#
+# 	# Change the indexes to dataset, filename
+# 	old_indexes = df.index.tolist()
+# 	old_indexes_split = [tuple(x.split('/')) for x in old_indexes]
+# 	filenames_df = pd.DataFrame(old_indexes_split, index=old_indexes, columns=['dataset', 'filename'])
+# 	df = pd.merge(df, filenames_df, left_index=True, right_index=True)
+# 	df = df.set_index(keys=['dataset', 'filename'])
+#
+# 	# Merge the two dataframes now that they have common indexes
+# 	final_df = df.join(acc_tables)
+# 	indexes_not_found = final_df[final_df.iloc[:, -len(acc_tables.columns):].isna().any(axis=1)].index.tolist()
+# 	print('Indexes not found in acc_tables file:')
+# 	[print(i, x) for i, x in enumerate(indexes_not_found)]
+#
+# 	# Save the final dataframe
+# 	final_df.to_csv(os.path.join(save_path, f'current_accuracy_{metric.upper()}.csv'), index=True)
+# 	print(final_df)
 
 
 def merge_scores_mts(path, metric, save_path, mts_metrics_path, mts_acc_tables_path):
@@ -214,23 +214,23 @@ def merge_inference_times(path, save_path):
 @hydra.main(config_path="conf", config_name="config.yaml")
 def main(cfg: DictConfig) -> None:
 	print(f'Merge scores with configuration: {cfg}')
-	if cfg.merge_score.mts == False:
-		merge_scores(
-			path=cfg.merge_score.path,
-			metric=cfg.merge_score.metrics[0],
-			save_path=cfg.merge_score_save_path,
+	# if cfg.merge_score.mts == False:
+	# 	merge_scores(
+	# 		path=cfg.merge_score.path,
+	# 		metric=cfg.merge_score.metrics[0],
+	# 		save_path=cfg.merge_score_save_path,
+	# 	)
+	# else:
+	metrics = cfg.merge_score.metrics
+	for m in metrics:
+		merge_scores_mts(
+			path=cfg.merge_score.raw_predictions_path,
+			metric=m,
+			save_path=cfg.merge_score.save_path,
+			mts_metrics_path=cfg.mts_current_metrics_path,
+			mts_acc_tables_path=cfg.mts_current_acc_tables_path,
 		)
-	else:
-		metrics = cfg.merge_score.metrics
-		for m in metrics:
-			merge_scores_mts(
-				path=cfg.merge_score.raw_predictions_path,
-				metric=m,
-				save_path=cfg.merge_score.save_path,
-				mts_metrics_path=cfg.mts_current_metrics_path,
-				mts_acc_tables_path=cfg.mts_current_acc_tables_path,
-			)
-			print(f'Merge scores metrics for {m}')
+		print(f'Merge scores metrics for {m}')
 
 
 	if cfg.merge_score.time:
