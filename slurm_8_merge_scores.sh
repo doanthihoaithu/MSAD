527e241b91b65ef39d1142915f2351d279f7888f
macbook <hoaithuxt1998@gmail.com>
2026-01-31 23:01:28 +0100

    do not use gpu for merge_scores

--- slurm_8_merge_scores.sh
+++ slurm_8_merge_scores.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=merge_scores
 #SBATCH --nodes=1 # 1 node
-#SBATCH --ntasks-per-node=16 # 32 tasks per node
+#SBATCH --ntasks-per-node=4 # 32 tasks per node
 #SBATCH --time=24:00:00 # time limits: 1 hour
 #SBATCH --error=slurm_logs/merge_scores.err # standard error file
 #SBATCH --output=slurm_logs/merge_scores.out # standard output file
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=cpu
 #SBATCH --partition=gprod_gssi # partition name
 #SBATCH --mail-type=END              # type of event notification
 #SBATCH --mail-user=thihoaithu.doan@gssi.it   # mail address
