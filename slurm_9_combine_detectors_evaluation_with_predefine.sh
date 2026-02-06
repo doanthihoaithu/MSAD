d5fd057227236acfda1545adb19ea9eca6dcec49
macbook <hoaithuxt1998@gmail.com>
2026-02-06 11:36:37 +0100

    update log for combine detectors with predefine

--- slurm_9_combine_detectors_evaluation_with_predefine.sh
+++ slurm_9_combine_detectors_evaluation_with_predefine.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodes=1 # 1 node
 #SBATCH --ntasks-per-node=16 # 32 tasks per node
 #SBATCH --time=24:00:00 # time limits: 1 hour
-#SBATCH --error=slurm_logs/combine_detectors.err # standard error file
-#SBATCH --output=slurm_logs/combine_detectors.out # standard output file
+#SBATCH --error=slurm_logs/combine_detectors_with_predefine.err # standard error file
+#SBATCH --output=slurm_logs/combine_detectors_with_predefine.out # standard output file
 #SBATCH --gres=gpu:a100:1
 #SBATCH --partition=gprod_gssi # partition name
 #SBATCH --mail-type=END              # type of event notification
