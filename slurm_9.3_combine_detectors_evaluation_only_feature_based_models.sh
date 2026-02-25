265e30297ae1eca113776e159464c7e8dfb5f93f
macbook <hoaithuxt1998@gmail.com>
2026-02-25 12:34:25 +0100

    add slurm scripts for combining detectors

---
+++ slurm_9.3_combine_detectors_evaluation_only_feature_based_models.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=combine_detectors_feature_based_models
+#SBATCH --nodes=1 # 1 node
+#SBATCH --ntasks-per-node=16 # 32 tasks per node
+#SBATCH --time=24:00:00 # time limits: 1 hour
+#SBATCH --error=slurm_logs/combine_detectors_feature_based_models.err # standard error file
+#SBATCH --output=slurm_logs/combine_detectors_feature_based_models.out # standard output file
+#SBATCH --partition=lprod # partition name
+#SBATCH --mail-type=END              # type of event notification
+#SBATCH --mail-user=thihoaithu.doan@gssi.it   # mail address
+
+module load python
+source /home/doan/projects/ibm_scripts/torch210/bin/activate
+python eval_combine_detectors_feature_based_models.py
+deactivate
